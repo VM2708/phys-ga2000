b27d596db7ae70090d01c700f5999e3ce1cdd5b6
VM2708 <vm2708@nyu.edu>
2024-09-06 12:01:19 -0400

    Create plot_gaussian.py

---
+++ PS1/plot_gaussian.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  5 14:07:52 2024
+
+@author: vedhasyamuvva
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def calculate_Gaussian(x):
+    a = 1 # height of the curve
+    b = 0 # position of the center of the curve
+    c = 3 #standard deviation
+    ans = (a * np.exp( (-(x-b)**2) / (2*c**2) ))
+    return ans
+
+# create x values to be plotted
+STEP_SIZE = 0.002 # step size of function
+xarray = np.arange(-10, 10, STEP_SIZE, dtype=np.float32)
+plt.plot(xarray, calculate_Gaussian(xarray))
+plt.title("Gaussian")
+plt.xlabel("bro")
+plt.text(-7.5,.1,r'bro')
+plt.savefig('gaussian.png')
+
+#What does normalizing mean???
